ca5166115b373a9212c6a38b195c7bb1290040f0
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-13 17:00:29 -0300

    Creating routes and page headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import {ThemeProvider} from "styled-components"
+import { ThemeProvider } from "styled-components"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
+
 import { GolbalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GolbalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+
+      <ThemeProvider theme={defaultTheme}>
+        <GolbalStyle />
+        
+        <Router />
+      </ThemeProvider>
+
+    </BrowserRouter>
   )
 }
