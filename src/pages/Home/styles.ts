f4c8d33f49f03bde9f576604f156216b273386eb
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-13 21:35:21 -0300

    Install React Hook Form

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -92,6 +92,7 @@ export const StartCountdownContainer = styled.button`
     border: 0;
     padding: 1rem;
     border-radius: 8px;
+    display: flex;
     align-items: center;
     justify-content: center;
 
